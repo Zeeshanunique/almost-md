2773921e13616f9f39da69bd34488c0eb91bae1c
Zeeshanunique <zeeshanunique2619@email.com>
2025-03-11 15:23:35 +0000

    Mar 11, 2025, 8:53 PM

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,26 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/"]
+  publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    // If the user is logged in and trying to access the home page,
+    // redirect them to /dashboard
+    if (auth.userId && req.nextUrl.pathname === "/") {
+      const dashboard = new URL("/dashboard", req.url);
+      return NextResponse.redirect(dashboard);
+    }
+
+    return NextResponse.next();
+  },
 });
 
+// Configure Middleware Matcher
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}; 
+};
