e32d8d7d14d207fdce7acefdeee477c9dc41c2d7
Zeeshanunique <zeeshanunique2619@email.com>
2025-03-11 15:39:28 +0000

    Mar 11, 2025, 9:09 PM

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
-import { auth } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { userId } = auth();
+  const user = await currentUser();
 
-  if (!userId) {
+  if (!user) {
     redirect("/sign-in");
   }
 
